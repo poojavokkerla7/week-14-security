6f3ae366827f35f96bc342c1112b465bf60ff438
poojavokkerla7 <pooja.vokkerla@gen2soft.com>
2026-06-26 11:01:20 -0700

    Updated Week14SecurityApplication.java

---
+++ src/main/java/com/gen2soft/week14security/Week14SecurityApplication.java
@@ -0,0 +1,11 @@
+package com.gen2soft.week14security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week14SecurityApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Week14SecurityApplication.class, args);
+    }
+}
